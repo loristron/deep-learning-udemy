4df78d76ea71106d745578770a6e83782f951786
loristron <loremmiranda@gmail.com>
2023-03-22 17:03:11 -0400

    Added problem description

---
+++ src/model.py
@@ -0,0 +1,5 @@
+# Criação de um modelo preditivo que, com base nos dados de treinamento, treine e 
+# entenda as correlações de todos os recursos e depois aplicar o modelo em diferentes clientes 
+# e elaborar uma predição de probabilidade do cliente sair do banco 
+
+
